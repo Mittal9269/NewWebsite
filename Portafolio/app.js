10cec2d6aabc89721ede57e3833f2a5886b933b2
Mittal9269 <72140620+Mittal9269@users.noreply.github.com>
2020-09-30 18:41:07 +0530

    Add files via upload

---
+++ Portafolio/app.js
@@ -0,0 +1,30 @@
+// console.log("cvv");
+let theme = localStorage.getItem('theme');
+if(theme == null){
+    setTheme('light');
+}else{
+    setTheme(theme);
+}
+let themeDot = document.getElementsByClassName('theme-dot');
+for(var i=0; themeDot.length>i; i++){
+    themeDot[i].addEventListener('click',function(){
+        var mode = this.dataset.mode;
+        // console.log('option clicked',mode);
+        setTheme(mode);
+    })
+}
+function setTheme(mode){
+    if(mode =='light'){
+        document.getElementById('theme-style').href = 'style.css';
+    }
+    if(mode =='blue'){
+        document.getElementById('theme-style').href = 'blue-theme.css';
+    }
+    if(mode =='green'){
+        document.getElementById('theme-style').href = 'green.css';
+    }
+    if(mode =='purple'){
+        document.getElementById('theme-style').href = 'purple.css';
+    }
+    localStorage.setItem('theme',mode);
+}
